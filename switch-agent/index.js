91cafa07303d68015a0418b01bc6bb14bc843c42
Andreas Voellmy <andreas.voellmy@gmail.com>
2015-03-01 11:27:34 -0500

    Fix indent.

--- switch-agent/index.js
+++ switch-agent/index.js
@@ -6,34 +6,34 @@ var _   = require('underscore');
 
 program
   .version('0.0.1')
-  .option('-s, --serverIP <type>', 'Server IP address')
-  .option('-p, --serverPort <n>', 'Server port', parseInt)
+  .option('-s, --server-ip <type>', 'Server IP address')
+  .option('-p, --server-port <n>', 'Server port', parseInt)
   .parse(process.argv);
 
-if (_.isUndefined(program.serverIP) || _.isUndefined(program.serverPort)) {
-    console.log("Missing command-line arguments");
-    console.log(process.argv.join(" "));
-    process.exit(1);
+if (_.isUndefined(program.serverIp) || _.isUndefined(program.serverPort)) {
+  console.log("Missing command-line arguments");
+  console.log(process.argv.join(" "));
+  process.exit(1);
 }
 
-console.log(program.serverIP);
+console.log(program.serverIp);
 console.log(program.serverPort);
 
 
 var client = 
     net.connect({
-	port: program.serverPort,
-	host: program.serverIP
+      port: program.serverPort,
+      host: program.serverIp
     }, function() { //'connect' listener
-	console.log('connected to server!');
-	client.write('GET / HTTP/1.1\r\n\r\n');
+      console.log('connected to server!');
+      client.write('GET / HTTP/1.1\r\n\r\n');
     });
 
 client.on('data', function(data) {
-    console.log(data.toString());
-    client.end();
+  console.log(data.toString());
+  client.end();
 });
 
 client.on('end', function() {
-    console.log('disconnected from server');
+  console.log('disconnected from server');
 });
