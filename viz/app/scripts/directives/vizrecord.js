e893123b304a24226d5bb565f57a057a62faf753
Jasson Casey <jasson@flowgrammable.com>
2015-03-02 19:19:35 -0500

    Playing with packet structure drawing

--- viz/app/scripts/directives/vizrecord.js
+++ viz/app/scripts/directives/vizrecord.js
@@ -6,25 +6,171 @@
  * @description
  * # vizRecord
  */
+
+function mkElement(box) {
+  var element = angular.element('<div></div>');
+  if(box.text) {
+    element.text(box.text);
+  }
+  element.css('width', box.width+'%');
+  element.css('float', 'left');
+  element.css('text-align', 'center');
+  element.css('font-style', 'italic');
+  element.css('margin-bottom', '-1px');
+  element.css('border-width', '1px');
+  element.css('border-style', 'solid');
+  return element;
+}
+
+function addBox(result, width, north, east, south, west) {
+  result.push({
+    width: width / 32 * 100,
+    north: north,
+    east: east,
+    south: south,
+    west: west
+  });
+}
+
+function mkPrefixBoxes(result, prefix, infix, postfix, field) {
+  var overlap;
+  if(infix > 0 || postfix == 32) {
+    addBox(result, prefix, true, true, false, true);
+  } else if(postfix > 0) {
+    overlap = postfix - field.col;
+    if(overlap > 0) {
+      addBox(result, overlap, true, false, false, true);
+      addBox(result, prefix-overlap, true, true, true, false);
+    } else {
+      addBox(result, prefix, true, true, true, true);
+    }
+  } else {
+    addBox(result, prefix, true, true, true, true);
+  }
+}
+
+function mkInfixBoxes(result, prefix, infix, postfix, field) {
+  // FIXME: this logic is busted
+  _(infix).times(function(i) {
+    if(i===0) {
+      if(prefix > 0) {
+        addBox(result, 32-prefix, true, false, infix > 1 ? false : true, true);
+        addBox(result, prefix, true, true, infix > 1 ? false : true, true);
+      } else {
+        if(postfix > 0) {
+          addBox(result, postfix, true, false, false, true);
+          addBox(result, 32-postfix, true, true, true, false);
+        } else {
+          addBox(result, 32, true, true, infix > 1 ? false : true, true);
+        }
+      }
+    } else if (i < infix - 1) {
+      addBox(result, 32, false, true, false, true);
+    } else {
+      if(postfix > 0) {
+        addBox(result, postfix, false, false, false, true);
+        addBox(result, 32-postfix, false, true, true, false);
+      } else {
+        addBox(result, 32, false, true, true, true);
+      }
+    }
+  });
+}
+
+function mkPostfixBoxes(result, prefix, infix, postfix, field) {
+  var overlap;
+  if(infix > 0 || prefix === 32) {
+    addBox(result, postfix, false, true, true, true);
+  } else if(prefix > 0) {
+    overlap = postfix - field.col;
+    if(overlap > 0) {
+      addBox(result, postfix-overlap, true, false, true, true);
+      addBox(result, overlap, false, true, true, false);
+    } else {
+      addBox(result, postfix, true, true, true, true);
+    }
+  } else {
+    addBox(result, postfix, true, true, true, true);
+  }
+}
+
+function mkBoxes(field) {
+  var overlap;
+  var result = [];
+  var prefix = 0;
+  var infix = 0;
+
+  if(field.bits + field.col > 32) {
+    prefix = 32 - field.col;
+  } else {
+    prefix = field.bits;
+  }
+  if(field.bits - prefix > 32) {
+    infix  = Math.floor((field.bits - prefix) / 32);
+  }
+  var postfix = field.bits - prefix - (32 * infix);
+
+  console.log('-------------------------');
+  console.log('Field: %s', field.name);
+  console.log('Prefix: %d', prefix);
+  console.log('Infix: %d', infix);
+  console.log('Postfix: %d', postfix);
+  console.log('-------------------------');
+
+  // A box starts above bit position 0
+  if(prefix > 0) {
+    mkPrefixBoxes(result, prefix, infix, postfix, field);
+  }
+  // A box starts at bit position 0 and ends at bit position 32
+  if(infix > 0) {
+    mkInfixBoxes(result, prefix, infix, postfix, field);
+  }
+  // A box ends before bit position 32
+  if(postfix > 0) {
+    mkPostfixBoxes(result, prefix, infix, postfix, field);
+  }
+  return result;
+}
+
+function setName(field, pixelWidth) {
+  var width, max;
+  max = _(field.boxes).max(function(box) { return box.width; });
+  width = max.width / 100 * pixelWidth;
+  if(field.name.length * 10 < width) {
+    max.text = field.name;
+  }
+}
+
+function prepare(fields, width) {
+  var pos = 0;
+  var max;
+  _(fields).each(function(field) {
+    field.row = Math.floor(pos / 32);
+    field.col = pos % 32;
+    pos += field.bits;
+    field.boxes = mkBoxes(field);
+    setName(field, width);
+  });
+}
+
 angular.module('vizApp')
   .directive('vizRecord', function () {
     return {
-      templateUrl: 'views/vizRecord.html',
       restrict: 'E',
       scope: {
         src: '='
       },
-      link: function(scope, element, attrs) {
+      link: function(scope, element) {
+        prepare(scope.src.fields, element.parent()[0].offsetWidth);
         _(scope.src.fields).each(function(field) {
-          var elem = angular.element('<div></div>');
-          var postfix = field.type === 'array' ? '[]' : '';
-          elem.text(field.name + postfix);
-          if(field.type === 'uint') {
-            elem.addClass('uint');
-          } else {
-            elem.addClass('composite');
-          }
-          element.append(elem);
+          console.log('--------------------');
+          console.log('Field: %s', field.name);
+          _(field.boxes).each(function(box) {
+            console.log('Box: %f - ', box.width, box.north, 
+                        box.east, box.south, box.west);
+            element.append(mkElement(box));
+          });
+          console.log('--------------------');
         });
       }
     };
